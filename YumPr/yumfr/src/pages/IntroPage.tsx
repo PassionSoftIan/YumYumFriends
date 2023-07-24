c96000f7c9e7586c1a8c31fa22dde0fad9c42372
protofu <sungjae0512@gmail.com>
2023-07-24 16:06:32 +0900

    230724

--- YumPr/yumfr/src/pages/IntroPage.tsx
+++ YumPr/yumfr/src/pages/IntroPage.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import logo from "../assets/startlogo.png";
-import "../style/IntroPage.css";
-
+import React, { useState, useEffect } from 'react';
+import IntroLogo from '../components/IntroPage/IntroLogo';
+import IntroCaution from '../components/IntroPage/IntroCaution';
 
 const IntroPage = () => {
-  const navigate = useNavigate();
+  const [showLogo, setShowLogo] = useState(true);
 
-  const handleButtonClick = () => {
-    // 버튼을 클릭했을 때 메인 페이지로 이동
-    navigate('/main');
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogo(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleCautionButtonClick = () => {
+    // IntroCaution의 확인 버튼을 눌렀을 때 처리하는 로직
+    // LoginPage로 이동
+    console.log('확인 버튼 클릭');
+    // 이 부분을 LoginPage로 이동하는 라우팅 로직으로 변경해야 합니다.
   };
 
   return (
-    <div className="intro-page">
-      <img src={logo} alt="Title Logo" className="logo" />
-
-      <h1 className="caution-heading">주의</h1>
-      <p className="caution-text">
-        유아가 이 애플리케이션을 사용하는 동안, 부모님의 동반 및 관찰이 필요합니다. 부모님과 함께 사용하면서 즐거운 시간을 보내는 것을 권장드립니다.
-      </p>
-      <button onClick={handleButtonClick} className="caution-button">확인</button>
+    <div>
+      {showLogo ? (
+        <IntroLogo />
+      ) : (
+        <IntroCaution onButtonClick={handleCautionButtonClick} />
+      )}
     </div>
   );
 };

4c115fd471778fa1db7e054c979ced1cc3363433
protofu <sungjae0512@gmail.com>
2023-07-25 14:31:26 +0900

    Feat:OpenVidu 디자인 추가

--- YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
+++ YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import OpenViduComponent from "../OpenVidu";
 
-const WebcamCapture: React.FC = () => {
+const SinglePlayPage: React.FC = () => {
+  // 여기에 싱글 플레이 페이지의 컨텐츠를 추가하세요
+  // 예를 들어 게임 컴포넌트와 게임 로직을 추가할 수 있습니다
+  // 이 주석을 실제 게임 컨텐츠로 대체하세요
+
   return (
-    <div>
-      <OpenViduComponent/>      
+    <div className="single-play-page">
+      <div className="game-container">
+        {/* 게임 컨텐츠를 구성하는 컴포넌트들을 추가하세요 */}
+      </div>
     </div>
   );
 };
 
-export default WebcamCapture;
+export default SinglePlayPage;

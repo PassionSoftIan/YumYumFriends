ba1e3d51d25ab7c1cd783592a92a642bd60eb3ea
DESKTOP-BLHP263\SSAFY <qjrm69@gmail.com>
2023-07-25 14:00:08 +0900

    Feat: 백엔드 REST API 서버 초안 초안 완성

--- YumPr/yumbe/src/main/java/com/ssafy/api/service/MyBadgeRepository.java
+++ YumPr/yumbe/src/main/java/com/ssafy/api/service/MyBadgeRepository.java
@@ -1,9 +1,17 @@
 package com.ssafy.api.service;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.api.model.MyBadge;
 
-public interface MyBadgeRepository extends JpaRepository<MyBadge, Integer>{
+@Repository
+public interface MyBadgeRepository extends JpaRepository<MyBadge, Integer> {
+	
+	void deleteByUserID(int userID);
+
+	List<MyBadge> findAllByUserID(int userID);
 
 }

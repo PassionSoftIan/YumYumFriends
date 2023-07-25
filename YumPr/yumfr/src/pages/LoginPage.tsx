2a20879ce50265cd1fdab2bfbfb9473cba262dd9
protofu <sungjae0512@gmail.com>
2023-07-25 11:14:03 +0900

    Feat:Dex 추가

--- YumPr/yumfr/src/pages/LoginPage.tsx
+++ YumPr/yumfr/src/pages/LoginPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import logo from "../assets/gametitle.png";
+import { useNavigate } from "react-router-dom";
 import "./styles/LoginPage.css";
 import SocialKakao from "../components/LoginPage/SocialKakao";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLoginSuccess = () => {
+    navigate("/main");
+  };
+
   return (
     <div className="login-page">
       <img src={logo} alt="Title Logo" className="gamelogo" />
-      <SocialKakao />
+      <SocialKakao onSuccess={handleLoginSuccess} /> {/* onSuccess 프로퍼티 전달 */}
     </div>
   );
 };

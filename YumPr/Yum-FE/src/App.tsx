61c0b2b74a4d6045232d39eb815020969c54f058
김동현 <kdhdh001@naver.com>
2023-07-25 12:03:26 +0900

    Fix:dedpage

--- YumPr/Yum-FE/src/App.tsx
+++ YumPr/Yum-FE/src/App.tsx
@@ -5,18 +5,12 @@ import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import SinglePlayPage from './pages/SinglePlayPage';
 import MultiPlayPage from './pages/MultiPlayPage';
-<<<<<<< HEAD:YumPr/Yum-FE/src/App.tsx
 // import OpenViduComponent from './OpenVidu';
-=======
-import DexPage from './pages/DexPage';
-import AnyComponent from './pages/any';
->>>>>>> 6fd216d1904aa4779f425dda2703195f75bc3110:YumPr/yumfr/src/App.tsx
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AnyComponent />
       <header className="App-header">
         <Router>
           <Routes>
@@ -25,7 +19,7 @@ function App() {
             <Route path="/main" element={<MainPage />} />
             <Route path="/single" element={<SinglePlayPage />} />
             <Route path="/multi" element={<MultiPlayPage />} />
-            <Route path="/dex" element={<DexPage />} />
+            {/* <Route path="/dex" element={<DexPage />} /> */}
           </Routes>
         </Router>
       </header>

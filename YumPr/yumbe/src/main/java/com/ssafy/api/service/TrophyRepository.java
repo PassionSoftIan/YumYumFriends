8b10a480863bd52eb8a25af435c8af450a21c7be
DESKTOP-BLHP263\SSAFY <qjrm69@gmail.com>
2023-07-25 10:56:13 +0900

    Docs: yumpr에 yumbe추가

---
+++ YumPr/yumbe/src/main/java/com/ssafy/api/service/TrophyRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.api.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.api.model.Trophy;
+
+@Repository
+public interface TrophyRepository extends JpaRepository<Trophy, Integer>{
+
+}

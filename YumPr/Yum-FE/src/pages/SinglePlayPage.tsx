6209e0ade9ff773f12f93bf0795ea11218e25f9e
김동현 <kdhdh001@naver.com>
2023-07-27 09:24:39 +0900

    Refactor: single, multi modify

--- YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
+++ YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles/SinglePlayPage.css";
+import OpenViduComponent from "../OpenVidu";
 
 const SinglePlayPage: React.FC = () => {
   return (
     <div className="single-play-page">
       <h1>Single Play Page</h1>
       {/* 여기에 내용 추가 */}
+      <OpenViduComponent/>
     </div>
   );
 };

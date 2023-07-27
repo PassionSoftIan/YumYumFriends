fd77d176a4adfad567576cd8c1303d90a85e947f
김동현 <kdhdh001@naver.com>
2023-07-27 17:56:19 +0900

    Feat: single com

--- YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
+++ YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./styles/SinglePlayPage.css";
 import OpenViduComponent from "../components/OpenVidu/OpenViduComponent";
 import BackImg from "../assets/background_kitchen.png";
+import Ham from "../assets/MovingYums/MovingHamburger.gif";
+import Bacteria from "../assets/MovingYums/MovingBacteria.gif";
 
 const SinglePlayPage: React.FC = () => {
   return (
@@ -12,6 +14,10 @@ const SinglePlayPage: React.FC = () => {
       </div>
       <div>
         <img src={BackImg} alt="" className="silge-play-footer-back" />
+        <div className="fightChars">
+          <img src={Ham} alt="내캐릭" className="MyCharacter" />
+          <img src={Bacteria} alt="세균" className="Bacteria" />
+        </div>
       </div>
     </div>
   );

6fbcfcd55d415b18da7085d661b39dc3bb493bad
김동현 <kdhdh001@naver.com>
2023-07-27 11:56:30 +0900

    Feat: openvidu singlepage on camera

--- YumPr/Yum-FE/src/components/OpenVidu/UserVideoComponent.js
+++ YumPr/Yum-FE/src/components/OpenVidu/UserVideoComponent.js
@@ -4,7 +4,7 @@ import "./UserVideo.css";
 
 export default class UserVideoComponent extends Component {
   getNicknameTag() {
-    // 사용자 닉네임 가져오기
+    // Gets the nickName of the user
     return JSON.parse(this.props.streamManager.stream.connection.data)
       .clientData;
   }
@@ -15,14 +15,6 @@ export default class UserVideoComponent extends Component {
         {this.props.streamManager !== undefined ? (
           <div className="streamcomponent">
             <OpenViduVideoComponent streamManager={this.props.streamManager} />
-            <div>
-                {this.props.streamManager !== undefined ? (
-                    <div className="streamcomponent">
-                        <OpenViduVideoComponent streamManager={this.props.streamManager} />
-                        {/* <div><p>{this.getNicknameTag()}</p></div> */}
-                    </div>
-                ) : null}
-            </div>
           </div>
         ) : null}
       </div>

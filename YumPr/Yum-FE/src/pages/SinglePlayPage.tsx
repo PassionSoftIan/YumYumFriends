8afe5424622b66902eff65211b2c580ae784ff8c
김동현 <kdhdh001@naver.com>
2023-07-27 15:26:47 +0900

    Feat: modify Nav

--- YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
+++ YumPr/Yum-FE/src/pages/SinglePlayPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles/SinglePlayPage.css";
 import OpenViduComponent from "../components/OpenVidu/OpenViduComponent";
+import BgImgBirth from "../assets/backgrond_birthday.png";
 
 const SinglePlayPage: React.FC = () => {
   return (

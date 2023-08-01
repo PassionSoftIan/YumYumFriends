27d1503c2790834448edc92d9f7698c4dd899d6e
protofu <sungjae0512@gmail.com>
2023-08-02 08:50:58 +0900

    Feat: Design Plus

--- frontend/src/pages/SinglePlayPage.tsx
+++ frontend/src/pages/SinglePlayPage.tsx
@@ -6,12 +6,8 @@ import BackImg from "../assets/background_kitchen.png";
 const SinglePlayPage: React.FC = () => {
   return (
     <div className="single-play-page">
-      {/* 여기에 내용 추가 */}
       <div>
         <OpenViduComponent />
-      </div>
-      <div>
-        <img src={BackImg} alt="" className="silge-play-footer-back" />
       </div>
     </div>
   );

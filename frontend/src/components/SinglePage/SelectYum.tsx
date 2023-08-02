5bc978bb29f06503e87a9104a352e82ccc358291
yoon-b <eunbi.kim.kor@gmail.com>
2023-08-02 17:39:31 +0900

    Feat: 냠냠 선택 화면 구성 중

---
+++ frontend/src/components/SinglePage/SelectYum.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import yums, { Yum } from "../../data/yums ";
+
+const SelectYum: React.FC = () => {
+  const acquiredYumIds = [1, 2, 4]; // Example array of acquired Yum IDs
+
+  return (
+    <React.Fragment>
+      <h3>함께 할 친구를 골라봐</h3>
+      {/* 여기에 캐릭터 선택 캐러셀 넣을거임 */}
+      <h3>왜 안나옴?</h3>
+    </React.Fragment>
+  );
+};
+
+export default SelectYum;

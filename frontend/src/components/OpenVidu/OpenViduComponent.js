675b6cfe687759139604fa33216cb7ea14684949
protofu <sungjae0512@gmail.com>
2023-08-03 15:50:26 +0900

    Feat : Merge comp
    Former-commit-id: 98019cb04be148b9f33177c5bd3cc924c9f9e936

--- frontend/src/components/OpenVidu/OpenViduComponent.js
+++ frontend/src/components/OpenVidu/OpenViduComponent.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import React, { Component } from "react";
 // import "./App.css";
 import UserVideoComponent from "./UserVideoComponent";
-import { div } from "@tensorflow/tfjs";
+
 
 const APPLICATION_SERVER_URL =
   process.env.NODE_ENV === "production" ? "" : "https://demos.openvidu.io/";

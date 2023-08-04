db7145334114c0cb34215ea358ee87a3f709bf9d
protofu <sungjae0512@gmail.com>
2023-08-04 09:05:46 +0900

    Feat : Kakao Login
    Former-commit-id: 51864862a4fe3890dd9acc210f3e6fc94a59bd8c

--- frontend/src/components/LoginPage/SocialKakao.tsx
+++ frontend/src/components/LoginPage/SocialKakao.tsx
@@ -1,38 +1,27 @@
-import React from "react";
-import KakaoLogin from "react-kakao-login";
-import { useNavigate } from "react-router-dom";
+import React, { FC } from 'react';
 
 interface SocialKakaoProps {
-  onSuccess: () => void;
+    onSuccess: () => void;  // Success handler prop
 }
 
-const SocialKakao: React.FC<SocialKakaoProps> = ({ onSuccess }) => {
-  const kakaoClientId = 'db4a66f215fd566fd6a8b24f9cfb4ef7'; // 카카오 개발자 사이트에서 발급받은 클라이언트 아이디로 변경
-  // const redirectUri = 'https://yumyumfriends.site/api/v1/kakao/login'; // 카카오 개발자 사이트에서 설정한 리다이렉트 URI로 변경
-  const navigate = useNavigate();
+const SocialKakao: FC<SocialKakaoProps> = ({ onSuccess }) => {
 
-  const kakaoOnSuccess = (response: any) => {
-    console.log(response);
-    // const code = response.response.code; // 카카오로부터 받은 인가 코드
-    // 백엔드로 인가 코드를 전달하고, 인가 코드를 사용하여 토큰을 발급받는 로직 구현
-    // 토큰 발급이 성공하면 아래의 onSuccess 함수 호출
-    onSuccess(); // 카카오 로그인이 성공했을 때, 전달받은 onSuccess 함수를 호출
-    navigate("/main"); // 페이지 이동
-  };
+    const KAKAO_OAUTH_URL = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=db4a66f215fd566fd6a8b24f9cfb4ef7&redirect_uri=https://yumyumfriends.site/api/v1/kakao/login';
 
-  const kakaoOnFail = (error: any) => {
-    console.log(error);
-  };
+    const handleLogin = () => {
+        window.open(
+            KAKAO_OAUTH_URL,
+            "KakaoLogin",
+            "width=500,height=500"
+        );
+        onSuccess();  // Call the success handler after opening the login window
+    };
 
-  return (
-    <>
-      <KakaoLogin
-        token={kakaoClientId} // 클라이언트 아이디 -> 토큰으로 변경
-        onSuccess={kakaoOnSuccess}
-        onFail={kakaoOnFail} // onFailure -> onFail로 변경
-      />
-    </>
-  );
+    return (
+        <button onClick={handleLogin}>
+            Login with Kakao
+        </button>
+    );
 };
 
 export default SocialKakao;

5e35a8d6f599248cf81eb0b2e89bcb6cd0b5383a
김동현 <kdhdh001@naver.com>
2023-08-07 15:59:21 +0900

    Feat: 자신이잡은도감확인 성공
    Former-commit-id: 0c5c9d27c21002c7ebbad5360b2cd8b31babb66a

--- frontend/src/components/SelectPage/SelectYum.tsx
+++ frontend/src/components/SelectPage/SelectYum.tsx
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "../Common/Carousel";
 import Button from "../Common/Button";
-import { useFetch } from "../../hooks/useFetch";
-import { string } from "@tensorflow/tfjs";
+import axios from "axios";
 
 // import yums, { Yum } from "../../data/yums ";
 
 const SelectYum: React.FC = () => {
-  const acquiredYumIds = [1, 2, 4]; 
+  const [yumList, setYumList] = useState([]);
+  const URL = "https://yumyumfriends.site";
+  const userID = localStorage.getItem("id");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      axios
+        .get(`${URL}/api/v1/collection/myyum`, { params: { user: userID } })
+        .then((data) => {
+          const mylist = data.data;
+          setYumList(mylist);
+          // console.log(yumList);
+        })
+        .catch((err) => console.log("실패"));
+    };
+    fetchData();
+  }, []);
 
   const handleSelectedYum = () => {
-    console.log('유저의 대표냠 설정 요청 보내기')
-  }
+    console.log("유저의 대표냠 설정 요청 보내기");
+  };
 
   return (
     <React.Fragment>
       <h3>함께 할 친구를 골라봐</h3>
       {/* 여기에 캐릭터 선택 캐러셀 넣을거임 */}
-      <Carousel carouselList={[
-        "1_tofu",
-        "2_mandarin",
-        "3_hamburger",
-        "4_icecream",
-        "5_radish"
-      ]} />
+      <Carousel carouselList={yumList} />
       <Button onClick={handleSelectedYum}>설정하기</Button>
     </React.Fragment>
   );

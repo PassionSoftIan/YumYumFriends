99cef308f618d3a20186e6a5dad360d915b5f3ca
protofu <sungjae0512@gmail.com>
2023-08-08 08:50:40 +0900

    Feat:Redux
    Former-commit-id: a566c0a235f5eed4270ab7ddfb095cf8d5fe8ee0

--- frontend/src/components/OpenVidu/GameStage.tsx
+++ frontend/src/components/OpenVidu/GameStage.tsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Button from "../Common/Button";
 import Banner from "../Common/Banner";
 import JSConfetti from "js-confetti";
 import { useNavigate } from "react-router-dom";
+import { setShowEffects } from "../../store/showEffectsSlice";
+import { RootState } from "../../store/store";
+
 
 const GameStage: React.FC = () => {
   const [eating, setEating] = useState(0);
   const [nowEating, setNowEating] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [showAnimation, setShowAnimation] = useState(false); // 상태 변수 추가
+  const [showAnimation, setShowAnimation] = useState(false);
+
   const maxEating: number = 3;
   const navigate = useNavigate();
 
+  // showEffects 상태를 가져오기 위해 useSelector 사용
+  const showEffects = useSelector((state: RootState) => state.showEffects.value);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    // 숟갈 유예기간
     if (nowEating) {
       const timeout = setTimeout(() => {
         setNowEating(false);
@@ -24,7 +32,6 @@ const GameStage: React.FC = () => {
   }, [nowEating]);
 
   useEffect(() => {
-    // showAnimation 상태 값이 변경될 때 애니메이션 시간(여기서는 2초) 후에 애니메이션 상태를 다시 false로 설정
     if (showAnimation) {
       const animationTimeout = setTimeout(() => {
         setShowAnimation(false);
@@ -33,6 +40,16 @@ const GameStage: React.FC = () => {
       return () => clearTimeout(animationTimeout);
     }
   }, [showAnimation]);
+
+  useEffect(() => {
+    if (showEffects) {
+      const effectsTimeout = setTimeout(() => {
+        dispatch(setShowEffects(false));
+      }, 2000);
+
+      return () => clearTimeout(effectsTimeout);
+    }
+  }, [showEffects, dispatch]);
 
   const handleButtonClick = () => {
     if (nowEating) {
@@ -56,17 +73,17 @@ const GameStage: React.FC = () => {
 
     setEating(eating + 1);
     setNowEating(true);
-    setShowAnimation(true); // 버튼이 클릭되면 애니메이션 시작 상태를 true로 설정
+    setShowAnimation(true);
+    dispatch(setShowEffects(!showEffects));
   };
 
   return (
     <div>
-      <React.Fragment>
-        {showModal && <Banner content="천천히 꼭꼭" />}
-        <Button onClick={handleButtonClick} className={showAnimation ? "animated-button" : ""}>
-          Click to Eat {eating}/{maxEating}
-        </Button>
-      </React.Fragment>
+      {showModal && <Banner content="천천히 꼭꼭" />}
+      <Button onClick={handleButtonClick} className={showAnimation ? "animated-button" : ""}>
+        Click to Eat {eating}/{maxEating}
+      </Button>
+      {showEffects && <div>이펙트가 보여집니다!</div>}
     </div>
   );
 };
--- frontend/src/pages/SinglePlayPage.tsx
+++ frontend/src/pages/SinglePlayPage.tsx
@@ -2,48 +2,59 @@ import React, { useState, useEffect } from "react";
 import "./styles/SinglePlayPage.css";
 import OpenViduComponent from "../components/OpenVidu/OpenViduComponent";
 import BackImg from "../assets/background_kitchen.png";
-import Ours from "../assets/before_fight/01_tofu_stand.gif"
-import Others from "../assets/before_fight/32_germ_standing.gif"
-import Effect from "../assets/effects/effect_1.png"
+import Ours from "../assets/before_fight/01_tofu_stand.gif";
+import Others from "../assets/before_fight/32_germ_standing.gif";
 
 const SinglePlayPage: React.FC = () => {
   const [showImages, setShowImages] = useState(true);
+  const [showEffects, setShowEffects] = useState(false);
 
   // 이미지들이 닿았을 때 처리하는 함수
   const handleImageTouch = () => {
     setShowImages(false);
+    setShowEffects(true);
   };
 
   useEffect(() => {
     const oursImageElement = document.getElementById("oursImage") as HTMLElement | null;
     const othersImageElement = document.getElementById("othersImage") as HTMLElement | null;
-  
+
     if (oursImageElement && othersImageElement) {
-      // 이미지들이 닿았을 때 처리를 위한 이벤트 리스너 추가
       oursImageElement.addEventListener("touchmove", handleImageTouch);
       othersImageElement.addEventListener("touchmove", handleImageTouch);
     }
-  
+
     return () => {
       if (oursImageElement && othersImageElement) {
-        // 컴포넌트가 언마운트되면 이벤트 리스너 제거
         oursImageElement.removeEventListener("touchmove", handleImageTouch);
         othersImageElement.removeEventListener("touchmove", handleImageTouch);
       }
     };
   }, []);
-  
+
+  // showEffects 상태가 변경될 때 애니메이션 시작
+  useEffect(() => {
+    if (showEffects) {
+      // 애니메이션 시간(여기서는 2초) 후에 showEffects 상태를 다시 false로 설정
+      const animationTimeout = setTimeout(() => {
+        setShowEffects(false);
+      }, 2000);
+
+      return () => clearTimeout(animationTimeout);
+    }
+  }, [showEffects]);
 
   return (
     <div className="single-play-page">
       <OpenViduComponent />
       {showImages && (
         <>
-           <img src={BackImg} alt="" className="overlay-image" />
-          <img src={Ours} alt="" className={`ours-image ${showImages ? '' : 'hidden'}`} id="oursImage" />
-          <img src={Others} alt="" className={`others-image ${showImages ? '' : 'hidden'}`} id="othersImage" />
-          <img src={Effect} alt="" className="effects-image" />        </>
+          <img src={BackImg} alt="" className="overlay-image" />
+          <img src={Ours} alt="" className={`ours-image ${showImages ? "" : "hidden"}`} id="oursImage" />
+          <img src={Others} alt="" className={`others-image ${showImages ? "" : "hidden"}`} id="othersImage" />
+        </>
       )}
+      {showEffects && <div className="effects-animation" />} {/* showEffects 상태가 true일 때 이펙트 애니메이션 보여줌 */}
     </div>
   );
 };

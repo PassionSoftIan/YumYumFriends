da86fb98e939894f74d3c8f770fd763d4161ded3
yoon-b <eunbi.kim.kor@gmail.com>
2023-08-07 15:17:56 +0900

    Feat: 플라스크 서버 요청
    Former-commit-id: 17dc18741491ef49c91fb9a4deb956e12223addc

--- frontend/src/components/SelectPage/SelectYum.tsx
+++ frontend/src/components/SelectPage/SelectYum.tsx
@@ -2,28 +2,29 @@ import React from "react";
 import Carousel from "../Common/Carousel";
 import Button from "../Common/Button";
 import { useFetch } from "../../hooks/useFetch";
-import { string } from "@tensorflow/tfjs";
 
 // import yums, { Yum } from "../../data/yums ";
 
 const SelectYum: React.FC = () => {
-  const acquiredYumIds = [1, 2, 4]; 
+  const acquiredYumIds = [1, 2, 4];
 
   const handleSelectedYum = () => {
-    console.log('유저의 대표냠 설정 요청 보내기')
-  }
+    console.log("유저의 대표냠 설정 요청 보내기");
+  };
 
   return (
     <React.Fragment>
       <h3>함께 할 친구를 골라봐</h3>
       {/* 여기에 캐릭터 선택 캐러셀 넣을거임 */}
-      <Carousel carouselList={[
-        "1_tofu",
-        "2_mandarin",
-        "3_hamburger",
-        "4_icecream",
-        "5_radish"
-      ]} />
+      <Carousel
+        carouselList={[
+          "1_tofu",
+          "2_mandarin",
+          "3_hamburger",
+          "4_icecream",
+          "5_radish",
+        ]}
+      />
       <Button onClick={handleSelectedYum}>설정하기</Button>
     </React.Fragment>
   );

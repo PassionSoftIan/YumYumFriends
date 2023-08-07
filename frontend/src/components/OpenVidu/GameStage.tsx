37eb7638892a03367fa1f13f278a22cc5a4c3e31
protofu <sungjae0512@gmail.com>
2023-08-07 17:55:20 +0900

    Feat:fight motion
    Former-commit-id: 3e6bc6ca39969b3a480d22fba060689ace59e8eb

--- frontend/src/components/OpenVidu/GameStage.tsx
+++ frontend/src/components/OpenVidu/GameStage.tsx
@@ -8,6 +8,7 @@ const GameStage: React.FC = () => {
   const [eating, setEating] = useState(0);
   const [nowEating, setNowEating] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showAnimation, setShowAnimation] = useState(false); // 상태 변수 추가
   const maxEating: number = 3;
   const navigate = useNavigate();
 
@@ -21,6 +22,17 @@ const GameStage: React.FC = () => {
       return () => clearTimeout(timeout);
     }
   }, [nowEating]);
+
+  useEffect(() => {
+    // showAnimation 상태 값이 변경될 때 애니메이션 시간(여기서는 2초) 후에 애니메이션 상태를 다시 false로 설정
+    if (showAnimation) {
+      const animationTimeout = setTimeout(() => {
+        setShowAnimation(false);
+      }, 2000);
+
+      return () => clearTimeout(animationTimeout);
+    }
+  }, [showAnimation]);
 
   const handleButtonClick = () => {
     if (nowEating) {
@@ -44,13 +56,14 @@ const GameStage: React.FC = () => {
 
     setEating(eating + 1);
     setNowEating(true);
+    setShowAnimation(true); // 버튼이 클릭되면 애니메이션 시작 상태를 true로 설정
   };
 
   return (
     <div>
       <React.Fragment>
         {showModal && <Banner content="천천히 꼭꼭" />}
-        <Button onClick={handleButtonClick}>
+        <Button onClick={handleButtonClick} className={showAnimation ? "animated-button" : ""}>
           Click to Eat {eating}/{maxEating}
         </Button>
       </React.Fragment>

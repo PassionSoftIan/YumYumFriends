dc4ec3140b1f51e2035391b13b6f5c2d9db41305
김동현 <kdhdh001@naver.com>
2023-08-08 16:51:09 +0900

    Feat: 랜덤 뽑기 진행중
    Former-commit-id: 7a703014c19749ae0268c448cf0a5eec131452b7

--- frontend/src/components/SinglePage/GetYum.tsx
+++ frontend/src/components/SinglePage/GetYum.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import GetStandingYum from "./GetStandingYum";
-import GetWalkingYum from "./GetWalkingYum";
 
 interface Yum {
   name: string;
@@ -12,22 +11,9 @@ interface Props {
 }
 
 const GetYum: React.FC<Props> = ({ yum }) => {
-  const [showStanding, setShowStanding] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowStanding(true);
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, []);
   return (
     <React.Fragment>
-      {showStanding ? (
-        <GetStandingYum yum={yum} />
-      ) : (
-        <GetWalkingYum yum={yum} />
-      )}
+      <GetStandingYum yum={yum} />
     </React.Fragment>
   );
 };

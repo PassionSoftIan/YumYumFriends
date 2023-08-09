425c9f9451c9ccc1ab25571bf35189299591877c
yoon-b <eunbi.kim.kor@gmail.com>
2023-08-09 10:09:47 +0900

    Feat: Redux-Persist 설치
    Former-commit-id: 3d950653dc4e260a16d34996afa802bd6741e3c6

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,18 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux'; // 추가
+
 import bgmReducer from './bgmSlice';
 import showEffectsReducer from './showEffectsSlice';
-import maxEatingReducer from "./maxEatingSlice";
+import maxEatingReducer from './maxEatingSlice';
 
-const store = configureStore({
-  reducer: {
-    bgm: bgmReducer,
-    showEffects: showEffectsReducer,
-    maxEating: maxEatingReducer,
-    // Add other reducers if needed
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  bgm: bgmReducer,
+  showEffects: showEffectsReducer,
+  maxEating: maxEatingReducer,
+  // Add other reducers if needed
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;

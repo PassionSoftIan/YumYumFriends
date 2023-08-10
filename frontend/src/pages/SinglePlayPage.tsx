a37b2bd871fa8a9c9c021c68b18ab132a428e26b
김동현 <kdhdh001@naver.com>
2023-08-10 10:35:22 +0900

    Feat: single플레이 바로 입장 및 세션ID, 참가자이름수정
    Former-commit-id: 83aac5e3d3657cf331526dc37ae0984300ee5a9b

--- frontend/src/pages/SinglePlayPage.tsx
+++ frontend/src/pages/SinglePlayPage.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store/store";
 import { setShowEffects, selectShowEffects } from "../store/showEffectsSlice";
 import "./styles/SinglePlayPage.css";
 import OpenViduComponent from "../components/OpenVidu/OpenViduComponent";
-import BackImg from "../assets/background_kitchen.png";
 import Ours from "../assets/before_fight/01_tofu_stand.gif";
 import Others from "../assets/before_fight/32_germ_standing.gif";
-import Effects from "../assets/effects/1_tofu.png"
-
+import Effects from "../assets/effects/1_tofu.png";
 
 const SinglePlayPage: React.FC = () => {
   const [showImages, setShowImages] = useState(true);
@@ -23,8 +21,12 @@ const SinglePlayPage: React.FC = () => {
   };
 
   useEffect(() => {
-    const oursImageElement = document.getElementById("oursImage") as HTMLElement | null;
-    const othersImageElement = document.getElementById("othersImage") as HTMLElement | null;
+    const oursImageElement = document.getElementById(
+      "oursImage"
+    ) as HTMLElement | null;
+    const othersImageElement = document.getElementById(
+      "othersImage"
+    ) as HTMLElement | null;
 
     if (oursImageElement && othersImageElement) {
       oursImageElement.addEventListener("touchmove", handleImageTouch);
@@ -46,7 +48,7 @@ const SinglePlayPage: React.FC = () => {
       const animationTimeout = setTimeout(() => {
         dispatch(setShowEffects(false));
       }, 1500);
-      console.log(showEffects)
+      console.log(showEffects);
       return () => clearTimeout(animationTimeout);
     }
   }, [showEffects, dispatch]);
@@ -56,12 +58,22 @@ const SinglePlayPage: React.FC = () => {
       <OpenViduComponent />
       {showImages && (
         <>
-          <img src={BackImg} alt="" className="overlay-image" />
-          <img src={Ours} alt="" className={`ours-image ${showImages ? "" : "hidden"}`} id="oursImage" />
-          <img src={Others} alt="" className={`others-image ${showImages ? "" : "hidden"}`} id="othersImage" />
+          <img
+            src={Ours}
+            alt=""
+            className={`ours-image ${showImages ? "" : "hidden"}`}
+            id="oursImage"
+          />
+          <img
+            src={Others}
+            alt=""
+            className={`others-image ${showImages ? "" : "hidden"}`}
+            id="othersImage"
+          />
         </>
       )}
-      {showEffects && <img src={Effects} alt="" className="effects-image" />} {/* showEffects 상태가 true일 때 이펙트 애니메이션 보여줌 */}
+      {showEffects && <img src={Effects} alt="" className="effects-image" />}{" "}
+      {/* showEffects 상태가 true일 때 이펙트 애니메이션 보여줌 */}
     </div>
   );
 };

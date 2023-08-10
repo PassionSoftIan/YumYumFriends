c6edea9287b06f12ee12304d938f1d640245bb47
protofu <sungjae0512@gmail.com>
2023-08-11 00:15:17 +0900

    Feat&Design:Detection Time & FightClub Background
    Former-commit-id: eb278136b8c73eb1dc64d28b268a33b58c502d1c

--- frontend/src/pages/SinglePlayPage.tsx
+++ frontend/src/pages/SinglePlayPage.tsx
@@ -5,29 +5,37 @@ import useImageSrc from "../hooks/useImage/useImageSrc";
 import useImageAttack from "../hooks/useImage/useImageAttack";
 import useImageEffect from "../hooks/useImage/useImageEffect";
 import { setShowEffects, selectShowEffects } from "../store/showEffectsSlice";
+
 import "./styles/SinglePlayPage.css";
 import OpenViduComponent from "../components/OpenVidu/OpenViduComponent";
-import BackImg from "../assets/playback.png";
-import Ours from "../assets/before_fight/01_tofu_stand.gif";
-import OursAttack from "../assets/AttackingYums/01_tofu_attack.gif";
+import RandomBack from "../hooks/useImage/useImageRandom";
 import Others from "../assets/before_fight/32_germ_standing.gif";
 import OthersAfterAttack from "../assets/Attacked/32_germ_attacked.gif";
 
 const SinglePlayPage: React.FC = () => {
   const [showImages, setShowImages] = useState(true);
-  const showEffects = useSelector(selectShowEffects); // Use the corrected selector
+  const [showEffectsTemp, setShowEffectsTemp] = useState(false); // 추가된 상태
+
+  const showEffects = useSelector(selectShowEffects);
+  const detection = useSelector((state: RootState) => state.detection);
   const dispatch = useDispatch();
   const ourImageSrc = useImageSrc();
   const ourImageAttack = useImageAttack();
   const ourImageEffect = useImageEffect();
-  const [mySession, setMySession] = useState(null);
-  const handleMySession = (obj:any) => { setMySession(obj); };
+  const useImageRandom = RandomBack();
+  const [mySession, setMySession] = useState<any>(null);
 
-  // 이미지들이 닿았을 때 처리하는 함수
+  const handleMySession = (obj: { eatValue: boolean }) => {
+    console.log("Received eatValue:", obj.eatValue);
+    if (obj.eatValue) {
+      setShowEffectsTemp(true); // eatValue가 true일 때 showEffectsTemp를 true로 설정
+    }
+    setMySession(obj);
+  };
+
   const handleImageTouch = () => {
     setShowImages(false);
-    // 버튼 클릭 시 리덕스 액션을 호출하여 showEffects 상태를 토글
-    dispatch(setShowEffects(!showEffects));
+    dispatch(setShowEffects(!showEffectsTemp)); // showEffectsTemp 값으로 변경
   };
 
   useEffect(() => {
@@ -49,38 +57,33 @@ const SinglePlayPage: React.FC = () => {
         othersImageElement.removeEventListener("touchmove", handleImageTouch);
       }
     };
-  }, [showEffects, dispatch]);
+  }, [showEffectsTemp, dispatch]);
 
-  // showEffects 상태가 변경될 때 애니메이션 시작
   useEffect(() => {
-    if (showEffects) {
-      // 애니메이션 시간(여기서는 2초) 후에 showEffects 상태를 다시 false로 설정
+    if (showEffectsTemp) {
       const animationTimeout = setTimeout(() => {
-        dispatch(setShowEffects(false));
+        setShowEffectsTemp(false); // 애니메이션이 끝나면 showEffectsTemp를 false로 설정
       }, 1500);
-      console.log(showEffects);
       return () => clearTimeout(animationTimeout);
     }
-  }, [showEffects, dispatch]);
+  }, [showEffectsTemp]);
+
 
   return (
     <div className="single-play-page">
-      <OpenViduComponent onObjectCreated= {handleMySession} />
+      <OpenViduComponent onObjectCreated={handleMySession} />
       <div className="images-container">
         {showImages && (
           <div className="images">
-            <img src={BackImg} alt="" className="overlay-image" />
+            <img src={useImageRandom} alt="" className="overlay-image" />
             <img
               src={showEffects ? ourImageAttack : ourImageSrc}
               alt=""
               className={`ours-image ${showImages ? "" : "hidden"}`}
               id="oursImage"
             />
-
             <img
-              src={
-                showEffects ? OthersAfterAttack : Others
-              } /* 애니메이션이 종료되면 OthersAfterAttack 이미지로 바꿔줌 */
+              src={showEffects ? OthersAfterAttack : Others}
               alt=""
               className={`others-image ${showImages ? "" : "hidden"}`}
               id="othersImage"

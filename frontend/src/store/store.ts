22d07f3d222d0fa6024e5a6aff6ddbfc04faecb3
yoon-b <eunbi.kim.kor@gmail.com>
2023-08-10 22:08:45 +0900

    Feat: HP bar
    Former-commit-id: 3c2ac534a2d1c783629302c8d04f8084db74061c

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -6,6 +6,7 @@ import { combineReducers } from 'redux';
 import bgmReducer from './bgmSlice';
 import showEffectsReducer from './showEffectsSlice';
 import maxEatingReducer from './maxEatingSlice';
+import eatingReducer from './eatingSlice';
 import userReducer from './userSlice';
 import detectionReducer from './detectionSlice';
 
@@ -20,6 +21,7 @@ const rootReducer = combineReducers({
   maxEating: maxEatingReducer,
   user: userReducer,
   detection: detectionReducer,
+  eating: eatingReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);

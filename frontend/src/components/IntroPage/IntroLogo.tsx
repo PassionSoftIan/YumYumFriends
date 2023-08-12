4d842c9285c2424b3eff671d65b5346d1247cc22
protofu <sungjae0512@gmail.com>
2023-08-12 21:01:56 +0900

    Design:IntroLogo Animation
    Former-commit-id: da68570e45b7440a5cee5b4b7fe910c843418136

--- frontend/src/components/IntroPage/IntroLogo.tsx
+++ frontend/src/components/IntroPage/IntroLogo.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/startlogo.png';
 import '../styles/IntroLogo.css';
 
 const IntroLogo = () => {
+  const [showLogo, setShowLogo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLogo(true);
+    }, 500); // 0.5초 후에 로고를 나타낸다
+  }, []);
+
   return (
-    <div className="intro-logo">
-      <img src={logo} alt="Title Logo" className="gamelogo" />
+    <div className={`intro-logo ${showLogo ? 'show' : ''}`}>
+      <img src={logo} alt="Title Logo" className="logo" />
     </div>
   );
 };

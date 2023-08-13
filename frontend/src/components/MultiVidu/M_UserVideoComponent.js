4dd03847f9ba0ab68e89f27f6e90228fa53361a9
권인식 <kwon2566@naver.com>
2023-08-13 21:20:10 +0900

    feat: OvVideo(My & Friend) 분기 bit 작성 및 영상 처리 컴포넌트 분리 |230813
    Former-commit-id: 13ff33c80a582db35e3ac7b4a3696ed89ad47d7f

--- frontend/src/components/MultiVidu/M_UserVideoComponent.js
+++ frontend/src/components/MultiVidu/M_UserVideoComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import M_OvVideo from "./M_OvVideo";
+import M_Friend_OvVideo from "./M_Friend_OvVideo";
 import "./UserVideo.css";
 
 export default class M_UserVideoComponent extends Component {
@@ -14,9 +15,15 @@ export default class M_UserVideoComponent extends Component {
       <React.Fragment>
         {this.props.streamManager !== undefined ? (
           <div className="streamcomponent" style={{width: "100%", height: "100%"}}>
-            <M_OvVideo
-              streamManager={this.props.streamManager}
-            />
+            {this.props.bit === 0 ? (
+              <M_OvVideo
+                streamManager={this.props.streamManager}
+              />
+            ) : (
+              <M_Friend_OvVideo
+                streamManager={this.props.streamManager}
+              />
+            )}
           </div>
         ) : null}
       </React.Fragment>

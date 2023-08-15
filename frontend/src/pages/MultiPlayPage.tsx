26cf9714d8baeeaa813f0590584c0f9649525bca
김동현 <kdhdh001@naver.com>
2023-08-16 01:11:22 +0900

    Fix: Multi 친구냠 띄우는중
    Former-commit-id: adcb08dd4c8a615ca6076319a2ff4d32ebd25ae4

--- frontend/src/pages/MultiPlayPage.tsx
+++ frontend/src/pages/MultiPlayPage.tsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 import M_OpenViduComponent from "../components/MultiVidu/M_OpenViduComponent";
+import "./styles/MultiPlayPage.css";
 // 여기부터 싱글 import
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store/store";
-// 여기까지 싱글 import
+// 얌얌이들
+import yum1 from "../assets/RunningYums/1.gif";
+import yum2 from "../assets/RunningYums/2.gif";
+import yum7 from "../assets/RunningYums/7.gif";
+import yum10 from "../assets/RunningYums/10.gif";
+import yum12 from "../assets/RunningYums/12.gif";
+import yum13 from "../assets/RunningYums/13.gif";
 
 const MultiPlayPage: React.FC = () => {
   const urlSearch = new URLSearchParams(window.location.search);
@@ -12,6 +19,16 @@ const MultiPlayPage: React.FC = () => {
   const hostInfo = decodeURIComponent(encodedHostInfo || "");
   const gameType = urlSearch.get("GameType");
   const myYum = localStorage.getItem("currentYum");
+  const yumImages: { [key: number]: string } = {
+    1: yum1,
+    2: yum2,
+    7: yum7,
+    10: yum10,
+    12: yum12,
+    13: yum13,
+  };
+
+  const [friendYum, setFriendYum] = useState(0);
 
   const commonProps = { sessionID, hostInfo, gameType };
 
@@ -20,12 +37,8 @@ const MultiPlayPage: React.FC = () => {
   const dispatch = useDispatch();
   const [mySession, setMySession] = useState<any>(null);
 
-  const eating = useSelector((state: RootState) => state.eating.value);
-  const maxEating = useSelector((state: RootState) => state.maxEating.value);
-
   const handleMySession = (obj: any) => {
     console.log("Received eatValue:", obj.eatValue);
-    console.log("머먹을지고르기기기기기기기");
     setMySession(obj);
     obj.on("signal:detection", (event: any) => {
       if (event.from.connectionId !== obj.connection.connectionId) {
@@ -42,12 +55,8 @@ const MultiPlayPage: React.FC = () => {
       console.log("test");
       if (event.from.connectionId !== obj.connection.connectionId) {
         console.log("FriendYum is :", event.data);
+        setFriendYum(parseInt(event.data));
         // 공격 메시지 수신 시 애니메이션 변경
-        if (event.data) {
-          // 공격 애니메이션 시작
-        } else {
-          // 공격 애니메이션 정지
-        }
       }
     });
   };
@@ -76,7 +85,7 @@ const MultiPlayPage: React.FC = () => {
       sendMessage(detection, "detection");
     }
   }, [detection]);
-
+  console.log(friendYum);
   return (
     <div className="Multi-play-page">
       {/* 멀티모드에서 화면 보이게 */}
@@ -86,6 +95,18 @@ const MultiPlayPage: React.FC = () => {
           onObjectCreated={handleMySession}
           {...commonProps}
         />
+        <img
+          className="myYum-image"
+          src={myYum ? yumImages[parseInt(myYum)] : yum1}
+          alt="My YUM"
+        />
+        {friendYum !== 0 && (
+          <img
+            className="friendYum-image"
+            src={yumImages[friendYum]}
+            alt="Friend YUM"
+          />
+        )}
       </div>
       {/* 멀티모드 애니메이션 */}
     </div>

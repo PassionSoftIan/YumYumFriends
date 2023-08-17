58cb125bbb89f1673f5279d14fafa011f934af48
yoon-b <eunbi.kim.kor@gmail.com>
2023-08-17 23:48:11 +0900

    Feat: 싱글 남은 횟수 띄우기
    Former-commit-id: f6f3deee385c94e4b852a5917ae8b809ddb543d5

--- frontend/src/components/ObservationVidu/O_UserVideoComponent.js
+++ frontend/src/components/ObservationVidu/O_UserVideoComponent.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
-import O_OvVideo from './O_OvVideo';
-import './UserVideo.css';
+import React, { Component } from "react";
+import O_OvVideo from "./O_OvVideo";
+import "./UserVideo.css";
 
 export default class O_UserVideoComponent extends Component {
+  getNicknameTag() {
+    // Gets the nickName of the user
+    return JSON.parse(this.props.streamManager.stream.connection.data)
+      .clientData;
+  }
 
-    getNicknameTag() {
-        // Gets the nickName of the user
-        return JSON.parse(this.props.streamManager.stream.connection.data).clientData;
-    }
-
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+        {this.props.streamManager !== undefined ? (
+          <div className="streamcomponent">
+            <O_OvVideo
+              streamManager={this.props.streamManager}
+              yumyum={this.props.yumyum}
+            />
             <div>
-                {this.props.streamManager !== undefined ? (
-                    <div className="streamcomponent">
-                        <O_OvVideo streamManager={this.props.streamManager} yumyum={this.props.yumyum} />
-                        <div><p>{this.getNicknameTag()}</p></div>
-                    </div>
-                ) : null}
+              <p>{this.getNicknameTag()}</p>
             </div>
-        );
-    }
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
